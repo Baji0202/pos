9a7c69230cf09f4345f339fb59d3c7875c153d58
Baji0202 <2021307319@dhvsu.edu.ph>
2024-04-01 23:05:38 +0800

    make receipt update

--- home.php
+++ home.php
@@ -300,6 +300,9 @@ const rowData = [];
 const customerName = document.getElementById('cfname').value;
 const customerEmail = document.getElementById('cemail').value;
 document.getElementById('cdetails').textContent = customerName+"  "+customerEmail;
+document.getElementById('mop').textContent = "Pay thru: Cash";
+        document.getElementById('amp').textContent = "Amount paid: ₱"+payment;
+document.getElementById('pchange').textContent = "Change: "+cchange;
 // Get the selected value of the discount dropdown
 const selectElement = document.getElementById('discount');
 
@@ -383,9 +386,7 @@ changecalc.addEventListener("click", () => {
             // Calculate and display the change
             const change = payment - gt;
             const cchange = document.getElementById('cchange').textContent = "₱" + parseFloat(change).toFixed(2);
-            document.getElementById('mop').textContent = "Pay thru: Cash";
-        document.getElementById('amp').textContent = "Amount paid: ₱"+payment;
-document.getElementById('pchange').textContent = "Change: "+cchange;
+            
         }
         
     } else {
