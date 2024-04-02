7959fcd8e097802c4dc0a79ed76ca6281f6ff7e3
Baji0202 <2021307319@dhvsu.edu.ph>
2024-04-02 17:15:58 +0800

    update

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 diko din gets code ko HAHa
+
+employee side acct
+j@gmail.com pass 123
+
+admin side acct
+neil@gmail.com pass 123
---
+++ admin_side/audit_trail.php
@@ -0,0 +1,51 @@
+<?php
+session_start();
+require_once "..\include\connect\dbcon.php";
+if (!isset($_SESSION['user_id'])) {
+    header("location: ..\index.php");
+}
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Audit Trail</title>
+</head>
+<body>
+    
+<table border='1' cellpadding='7'>
+    <tr>
+    <th>ID</th>
+    <th>Action</th>
+    <th>Timestamps</th>
+    <th>User_ID</th>
+    </tr>
+    
+    <?php 
+    require_once "..\include\connect\dbcon.php";
+   
+    $sql = "SELECT * FROM log";
+    $stmt = $pdoConnect->prepare($sql);
+    $stmt->execute();
+
+    while ($row = $stmt->fetch(PDO::FETCH_ASSOC)) {
+        extract($row);
+
+        echo "<tr>";
+        echo "<td>" . $id . "</td>";
+        echo "<td>" . $action . "</td>";
+        echo "<td>" . $timestamp . "</td>";
+        echo "<td>" . $user_id ."</td>";
+        echo "</tr>";
+    }
+    echo '</table>';
+    
+?>
+
+
+
+</table>
+</body>
+</html>
