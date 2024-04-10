fae94eed2f7e3448326e9ae3f467c837a965593f
Neil Alfred Lapuz <lapuzneilalfredbusiness@gmail.com>
2024-04-10 20:45:12 +0800

    hrfgjngfd

--- admin_side/discount.php
+++ admin_side/discount.php
@@ -11,7 +11,8 @@ if (!isset($_SESSION['user_id'])){
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="icon" type="image/png" href="include\image\logo.png">
+    <link rel="icon" type="image/png" href="..\include\image\logo.png">
+    <link rel="stylesheet" href="../include/styles/discount.css">
     <title>Discounts</title>
 </head>
 <body>
