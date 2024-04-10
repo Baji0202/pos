2e935f8cf4aa2388d14050d4126f6f681a186d51
Neil Alfred Lapuz <lapuzneilalfredbusiness@gmail.com>
2024-04-10 21:37:29 +0800

    fdsef
    dsf

--- admin_side/discount_add.php
+++ admin_side/discount_add.php
@@ -39,6 +39,8 @@ if (isset($_POST['add'])) {
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Discount Update</title>
+    <link rel="icon" type="image/png" href="..\include\image\logo.png">
+    <link rel="stylesheet" href="../include/styles/discount_update.css">
 </head>
 <body>
 
