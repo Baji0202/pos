892699e3dc9af9628ac385173fcfecadd4e30dcb
Baji0202 <2021307319@dhvsu.edu.ph>
2024-04-26 09:57:14 +0800

    email

--- sendmail.php
+++ sendmail.php
@@ -25,23 +25,25 @@ $customerName = $emailData['name'];
 $customerEmail = $emailData['email'];
 $pdfContent = $emailData['pdfContent'];
 
-var_dump($pdfContent);
 
-sendEmailWithAttachment($customerEmail, $customerName, $pdfContent);
-if (sendEmailWithAttachment($customerEmail, $customerName, $pdfContent)) {
-  echo"email sent successfully";
+$emailSent = sendEmailWithAttachment($customerEmail, $customerName, $pdfContent);
+
+if ($emailSent) {
+    echo "Email sent successfully";
 }
+
 function sendEmailWithAttachment($email, $name, $pdfContent) {
     try {
         $mail = new PHPMailer(true);
-        $mail->SMTPDebug = SMTP::DEBUG_OFF;                      //Enable verbose debug output
-        $mail->isSMTP();                                            //Send using SMTP
-        $mail->Host       = 'smtp.gmail.com';                     //Set the SMTP server to send through
-        $mail->SMTPAuth   = true;                                   //Enable SMTP authentication
-        $mail->Username   = 'posbsite3e@gmail.com';                     //SMTP username
-        $mail->Password   = 'aded ceoh xlxz qhwg';                               //SMTP password
-        $mail->SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS;            //Enable implicit TLS encryption
-        $mail->Port       = 587;                               
+        $mail->SMTPDebug = SMTP::DEBUG_OFF; //Enable verbose debug output
+        $mail->isSMTP(); //Send using SMTP
+        $mail->Host = 'smtp.gmail.com'; //Set the SMTP server to send through
+        $mail->SMTPAuth = true; //Enable SMTP authentication
+        $mail->Username = 'posbsite3e@gmail.com'; //SMTP username
+        $mail->Password = 'aded ceoh xlxz qhwg'; //SMTP password
+        $mail->SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS; //Enable implicit TLS encryption
+        $mail->Port = 587; 
+
         // Set email parameters
         $mail->setFrom('posbsite3e@gmail.com', 'BANANA WEAR?');
         $mail->addAddress($email, $name);
@@ -51,15 +53,15 @@ function sendEmailWithAttachment($email, $name, $pdfContent) {
         $mail->Body = $pdfContent;
         $mail->AltBody = 'Please find attached your receipt.';
 
-        
-        // $mail->addStringAttachment($pdfContent, 'receipt.pdf', 'base64', 'application/pdf');
+        // $mail->addStringAttachment($pdfContent, 'receipt.pdf', 'base64', 'application/pdf'); // Uncomment to attach PDF
 
         // Send email
         $mail->send();
-        echo 'Email sent successfully';
+        return true; // Indicate successful sending
     } catch (Exception $e) {
         http_response_code(500);
         echo "Message could not be sent. Mailer Error: {$mail->ErrorInfo}";
+        return false; // Indicate error
     }
 }
 ?>
