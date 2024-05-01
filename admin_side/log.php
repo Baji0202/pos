d2b3495082589aeb2632997c6c544afd8cf6cb7d
Neil Alfred Lapuz <154073022+sagingsaba@users.noreply.github.com>
2024-05-02 02:28:31 +0800

    cama

--- admin_side/log.php
+++ admin_side/log.php
@@ -1,5 +1,5 @@
 <?php
-function loghistory($pdoConnect,$message,){
+function loghistory($pdoConnect,$message){
     $timestamp = date('Y-m-d H:i:s');
     $sql = "INSERT INTO `log`(`action`, `timestamp`, `user_id`) VALUES (?,?,?)";
     $stmt = $pdoConnect->prepare($sql);
