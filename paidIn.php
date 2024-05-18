1b7101267661eb3262557e1cb383927f5cd5688c
Neil Alfred Lapuz <lapuzneilalfredbusiness@gmail.com>
2024-05-18 10:14:08 +0800

    sabi nga ni pou: nyak

--- paidIn.php
+++ paidIn.php
@@ -17,19 +17,54 @@ if (isset($_POST['submit'])) {
     
 }
 
+
+$loggedemail = $_SESSION['email'];
+require_once "include\connect\dbcon.php";
+
+
+
 ?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="include\styles\global.css">
     <title>Paid In</title>
 </head>
 <body>
-    <form  method="post">
-<label>Add paid in</label>
+<nav>
+    <div class="logo">
+    <img src="include\image\sadas.png" alt="Company Logo" class="logo_pic">
+        <div class="text_logo">POS System</div>
+        <link rel="stylesheet" href="include\styles\global.css">
+    </div>
+    
+    <div class="dropdown">
+        <div class="acc_name" id="acc_name"><?php echo $loggedemail?></div>
+        <!-- Dropdown content -->
+        <div class="dropdown-content" id="logout_dropdown">
+            <a href="cashmanagement.php">Back</a>
+            <a href="home.php">Home</a>
+            <a href="refund.php">Refund</a>
+            <a href="logout.php" class="logout-btn">Logout</a>
+            
+        </div>
+    </div>
+</nav>
+
+
+
+
+
+<div class="maincontainer">
+<div class ="form" style="margin-top:3%;">
+<form  method="post">
+<h2 style=" display: block; text-align: center; margin-bottom: 12%; ">Add paid in</h2>
 <input type="text" name="paidIn" pattern="\d+(\.\d{1,2})?" oninput="this.value = this.value.replace(/[^\d.]/g, '');" required placeholder="0.00">
 <input type="submit" value="submit" name="submit">
     </form>
+</div>
+</div>
 </body>
 </html>
--- paidOut.php
+++ paidOut.php
@@ -25,10 +25,36 @@ if (isset($_POST['submit'])) {
     <title>Paid Out</title>
 </head>
 <body>
-    <form  method="post">
-<label>Add paid in</label>
+
+<nav>
+    <div class="logo">
+    <img src="include\image\sadas.png" alt="Company Logo" class="logo_pic">
+        <div class="text_logo">POS System</div>
+        <link rel="stylesheet" href="include\styles\global.css">
+    </div>
+    
+    <div class="dropdown">
+        <div class="acc_name" id="acc_name"><?php echo $loggedemail?></div>
+        <!-- Dropdown content -->
+        <div class="dropdown-content" id="logout_dropdown">
+            <a href="cashmanagement.php">Back</a>
+            <a href="home.php">Home</a>
+            <a href="refund.php">Refund</a>
+            <a href="logout.php" class="logout-btn">Logout</a>
+            
+        </div>
+    </div>
+</nav>
+
+
+<div class="maincontainer">
+<div class ="form" style="margin-top:3%;">
+<form  method="post">
+<h2 style=" display: block; text-align: center; margin-bottom: 12%; ">Add paid out</h2>
 <input type="text" name="paidOut" pattern="\d+(\.\d{1,2})?" oninput="this.value = this.value.replace(/[^\d.]/g, '');" required placeholder="0.00">
 <input type="submit" value="submit" name="submit">
     </form>
 </body>
+</div>
+</div>
 </html>
