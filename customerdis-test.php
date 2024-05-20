1c6c8d6ae91c00ade596f72211be5e05457ef5ad
Neil Alfred Lapuz <lapuzneilalfredbusiness@gmail.com>
2024-05-20 14:31:25 +0800

    test

--- customerdis-test.php
+++ customerdis-test.php
@@ -58,6 +58,8 @@ $(document).ready(function() {
   <p>Product: <span id="product-name"></span></p>
   <p>Price: <span id="price"></span></p>
   <p>Quantity: <span id="quantity"></span></p>
+
+  test
 </div>
 </body>
 </html>
